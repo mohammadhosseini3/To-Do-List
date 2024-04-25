875550df9e752c000961a159c24a1dd9e49c157d
mohmmadhosseini3 <mohammadhhosseini286@gmail.com>
2024-04-25 22:51:28 +0300

    adding task updated

--- App.js
+++ App.js
@@ -1,18 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+import React , {useState} from "react"
 import Task from "./components/Task"
+import { StyleSheet, Text, View ,TextInput, TouchableOpacity } from 'react-native'
+
 
 export default function App() {
+
+  const [task,setTask] = useState('')
+  const [taskItems,setTaskItems] = useState([])
+
+  const handleAddTask = (task) => {
+    console.log(task)
+    // setTaskItems([...taskItems,task])
+    // setTask(null)
+  }
+
   return (
     <View style={styles.container}>
       {/* Today's Task  */}
       <View style={styles.taskWrapper}>
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
         <View style={styles.items}>
+          {
+            taskItems.map((item) => {
+              return <Task text={item}/>
+            })
+          }
           <Task text="Task 1"/>
-          <Task text="Task 2"/>
-          <Task text="Task 3"/>
         </View>
       </View>
+
+      {/* Write a task */}
+      <View style={styles.taskInput}>
+        <TextInput style={styles.textInput} placeholder='Write a task ...' onChangeText={text => setTask(text)} value={task}></TextInput>
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.btnInput}>
+            <Text>+</Text>
+          </View>
+        </TouchableOpacity>
+        
+      </View>
+
     </View>
   )
 }
@@ -32,5 +60,33 @@ const styles = StyleSheet.create({
   },
   items:{
     marginTop:20
+  },
+  taskInput:{
+    flexDirection:"row",
+    justifyContent:"space-around",
+    position:'absolute',
+    bottom:60,
+    alignItems:"center",
+    width:"100%"
+  },
+  btnInput:{
+    width:50,
+    height:50,
+    borderRadius:60,
+    backgroundColor:"white",
+    alignItems:"center",
+    justifyContent:"center",
+    borderColor:"#372D73",
+    borderWidth:1,
+  },
+  textInput:{
+    borderRadius:60,
+    backgroundColor:"white",
+    marginLeft:20,
+    height:50,
+    width:250,
+    padding:15,
+    borderColor:"#372D73",
+    borderWidth:1,
   }
 });
